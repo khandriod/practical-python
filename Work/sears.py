b27e5093ca19af80020d373916d8aa001a483754
khandriod <khandriod@gmail.com>
2022-03-19 15:13:14 +0900

    first file

---
+++ Work/sears.py
@@ -0,0 +1,16 @@
+# bill_thickness = 0.11 * 0.001 #meters
+# sears_height = 442 #Height in meters
+# num_bills = 1
+# day = 1
+
+# while num_bills * bill_thickness < sears_height:
+#     print(day, num_bills, num_bills * bill_thickness)
+#     day = day + 1
+#     num_bills = num_bills * 2
+
+# print('Number of day', day)
+# print('Number of bills', num_bills)
+# print('Final height', num_bills * bill_thickness)
+
+print('Hello', end=' ')
+print('My name is', 'Jake')
