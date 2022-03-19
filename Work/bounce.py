4915f8e1b54a1777d7857cec6c78f70e18e2e806
khandriod <khandriod@gmail.com>
2022-03-19 15:46:22 +0900

    first ex

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
+import random
+
+height = 100 #meters
+bounce = 1
+
+while bounce < 10:
+    height = height * 3/5
+    print(bounce, round(height, 4))
+    bounce = bounce + 1
+
 # Exercise 1.5
